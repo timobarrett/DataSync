8cc23938196ffbfb24e5b60b10cda9a516d7f628
timobarrett <timobarrett@gmail.com>
2017-03-31 14:33:13 -0400

    Create Application.java
    Spring boot application module

---
+++ Server Source/Application.java
@@ -0,0 +1,17 @@
+package fivetwentysix.ware.com.dataSync;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by tim on 1/29/2017.
+ */
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class Application {
+
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
+    }
